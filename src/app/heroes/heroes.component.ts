7a31f7fa37d9aa6e37db39329ca72a93b1a8116e
VickaMarkina <marckina.vika99@gmail.com>
2021-11-25 12:40:42 +0200

    Create a Feature Component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../shared/hero';
 import { HEROES } from '../shared/mock-heroes';
 
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.scss']
 })
 
 export class HeroesComponent implements OnInit {
